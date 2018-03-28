463ea83409214ee720667da94f950e08f7ab8253
Josip Tomaić <tomaic.josip@hotmail.com>
2018-03-28 16:19:54 +0200

    added Facebook sharing option to discount details screen

--- myreactapp/src/modules/discounts/discount_details/components/DiscountDetails.tsx
+++ myreactapp/src/modules/discounts/discount_details/components/DiscountDetails.tsx
@@ -1,7 +1,8 @@
 import * as React from 'react';
-import { Panel, ListGroup, ListGroupItem, Button } from 'react-bootstrap';
+import { Panel, ListGroup, ListGroupItem, Button, Modal } from 'react-bootstrap';
 import { GetSpecificDiscount } from '../../../discounts';
 import QrReader from 'react-qr-reader';
+import { FacebookShareButton, FacebookIcon } from 'react-share';
 import '../DiscountDetails.css';
 
 const regularPrice = 'Then';
@@ -15,7 +16,8 @@ export class DiscountDetails extends React.Component<any, any> {
             showCamera: false,
             delay: 300,
             result: 'No result',
-            cameraType: 'environment'
+            cameraType: 'environment',
+            showSharing: false
         };
     }
 
@@ -56,6 +58,14 @@ export class DiscountDetails extends React.Component<any, any> {
         } else {
             this.setState({ cameraType: 'user' });
         }
+    }
+
+    handleClose = () => {
+        this.setState({ showSharing: false });
+    }
+
+    handleShow = () => {
+        this.setState({ showSharing: true });
     }
 
     goHome = () => {
@@ -103,9 +113,24 @@ export class DiscountDetails extends React.Component<any, any> {
                         <p>{this.state.result}</p>
                         <Button onClick={this.close}>Close QR Code Reader</Button>
                         <Button onClick={this.changeCamera.bind(this, this.state.cameraType)}>Change camera</Button>
-                        <Button disabled={this.state.result === 'No result'} onClick={this.redeemDiscount.bind(this, discount.discountPrice)}>
+                        <Button disabled={this.state.result === 'No result'} onClick={this.handleShow}>
                             Redeem discount
                         </Button>
+                        <Modal show={this.state.showSharing} onHide={this.handleClose}>
+                            <Modal.Header closeButton>
+                                <Modal.Title>Social share</Modal.Title>
+                            </Modal.Header>
+                            <Modal.Body>
+                                <FacebookShareButton url={'http://github.com'} title={'GitHub'}>
+                                    <h3>Share:</h3>
+                                    <FacebookIcon size={32} round/>
+                                </FacebookShareButton>
+                            </Modal.Body>
+                            <Modal.Footer>
+                                <Button onClick={this.handleClose}>Close</Button>
+                                <Button onClick={this.redeemDiscount.bind(this, discount.discountPrice)}>Confirm</Button>
+                            </Modal.Footer>
+                        </Modal>
                     </div>
                 );
             }
